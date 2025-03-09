51ae3974234ec03044bb81c43e291e3b15443f65
Ravindra wayal <113979072+RavindraWayal@users.noreply.github.com>
2025-03-09 10:08:35 +0530

    Add files via upload

---
+++ productlist/src/test/java/com/practise/productlist/ProductlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practise.productlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
